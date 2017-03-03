49845c71622c42fe9ee69c8ab18e583cac487c6a
wictus <wictusn@gmail.com>
2017-03-03 14:23:01 +0100

    Add example with different smearings

--- main.cpp
+++ main.cpp
@@ -3,35 +3,77 @@
 #include "energySpectrum.h"
 #include "TH1F.h"
 #include "TCanvas.h"
+#include "TLegend.h"
 #include <sstream>
+#include <vector>
 
-int main(int argc, char **argv) {
-    
-  energySpectrum petSpectrumPure(511, 0);
-  petSpectrumPure.setNumberOfEvents(1E7);
-  petSpectrumPure.generateEvents();
-  TH1F* pure511 = petSpectrumPure.plotHisto();   
-  TCanvas* c = new TCanvas();
-  pure511->Draw();
-  c->SaveAs("pure.png");
-  delete c;
-  for(double i = 0; i < 2.0; i += 0.1)
+void smearingComparison()
+{
+  TCanvas* c = new TCanvas();;
+  std::vector<TH1F*> histosToPlot;
+  for(double i = 0; i < 2.0; i += 0.5)
   {
-    c = new TCanvas();
     energySpectrum kiko(511, i);
-    kiko.setNumberOfEvents(1E7);
+    kiko.setNumberOfEvents(1E8);
     kiko.generateEvents();
     TH1F* histoik = kiko.plotHisto();
-    histoik->Draw();
-    std::stringstream lako;
-    lako << i;
-    
-    c->SaveAs( (lako.str() + ".png").c_str() );
-    lako.clear();
-    delete histoik;
-    delete c;
+    histosToPlot.push_back(histoik);
   }
   
+  histosToPlot[0]->Draw();
+  for( int i = 1; i < histosToPlot.size(); i++)
+  {
+    histosToPlot[i]->SetLineColor(i+1);
+    histosToPlot[i]->Draw("same");
+  }
+    
+  c->SaveAs("smearing.root");
+}
+
+void initalEnergyComparison()
+{
+  TCanvas* c = new TCanvas();;
+  std::vector<TH1F*> histosToPlot;
+  std::vector<double> energies;
+  energies.push_back(340);
+  energies.push_back(450);
+  energies.push_back(511);
+  std::vector<double> angles;
+  angles.push_back(60);
+  angles.push_back(30);
+  angles.push_back(0);
+  for(int i = 0; i < energies.size(); i++)
+  {
+    energySpectrum kiko(energies[i], 0);
+    kiko.setNumberOfEvents(1E8);
+    kiko.generateEvents();
+    TH1F* histoik = kiko.plotHisto();
+    std::stringstream buf;
+    buf << energies[i] ;
+    buf << + " keV (#theta = ";
+    buf << angles[i];
+    buf << "^{o})";
+    std::string name = "Initial energy " + buf.str()  ;
+    histoik->SetName( name.c_str() );
+    histoik->SetTitle( name.c_str() );
+    histosToPlot.push_back(histoik);
+    buf.clear();
+  }
+  
+  histosToPlot[0]->Draw();
+  for( int i = 1; i < histosToPlot.size(); i++)
+  {
+    histosToPlot[i]->SetLineColor(i+1);
+    histosToPlot[i]->Draw("same");
+  }
+    
+  c->SaveAs("energies.root");
+}
+
+
+int main(int argc, char **argv) {
+//   smearingComparison();
+  initalEnergyComparison();
   
     return 0;
 }
